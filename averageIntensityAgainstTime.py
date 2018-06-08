318b80313c82c5c9c4514decbb9770f5f7d7d19e
arianegouin <39052797+arianegouin@users.noreply.github.com>
2018-06-08 08:56:28 -0400

    essai

--- averageIntensityAgainstTime.py
+++ averageIntensityAgainstTime.py
@@ -7,18 +7,23 @@ import os
 
 class CSVFile:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.directory = directory = os.fsencode(directory_in_str)
+        self.filename = os.path.basename(root.PathAndFilename)
 
-    @staticmethod
-    def getFilePath():
-        root = tk.Tk()
-        root.withdraw()
-        root.PathAndFilename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select HDF5 file",
-                                                          filetypes=(("CSVfiles", "*.csv"), ("all files", "*.*")))
-        directory = os.path.split(root.PathAndFilename)[0] + '/'
-        fileName = os.path.basename(root.PathAndFilename)
-        return directory + fileName
+    def iterateCSVThroughFolder(self):
+        # directory = CSVFile.getFilePath()[0]
+        # print(directory)
+        for file in os.listdir(self.directory):
+            filename = os.fsdecode(file)
+            if filename.endswith(".csv"):
+                print('ok')
+                print(os.path.join(self.directory, filename))
+                # return os.path.join(directory, filename)
+                continue
+            else:
+                print('no')
+                continue
 
     def read(self):
         with open(self.name) as file:
@@ -33,11 +38,20 @@ class CSVFile:
             try:
                 float(row[0])
             except ValueError:
-                pass
+                continue
             else:
                 print(row)
             yield xdata, ydata
 
-x = CSVFile('Results.csv')
-for i in x.read():
-    print(i)
+
+root = tk.Tk()
+root.withdraw()
+root.PathAndFilename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select HDF5 file",
+                                                  filetypes=(("CSVfiles", "*.csv"), ("all files", "*.*")))
+directory = os.path.split(root.PathAndFilename)[0] + '/'
+filename = os.path.basename(root.PathAndFilename)
+
+print(filename)
+
+# for i in x.read():
+#     print(i)

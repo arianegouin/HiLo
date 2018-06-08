74fbe460ef63c8f2f4f0ff5905f26694710b16cc
arianegouin <39052797+arianegouin@users.noreply.github.com>
2018-06-08 11:19:04 -0400

    change a lot

--- averageIntensityAgainstTime.py
+++ averageIntensityAgainstTime.py
@@ -3,55 +3,74 @@ from matplotlib import pyplot as plt
 import tkinter as tk
 from tkinter import filedialog
 import os
+import numpy
+
+
+class Folder:
+
+    def __init__(self):
+        root = tk.Tk()
+        root.withdraw()
+        root.directory = filedialog.askdirectory(initialdir=os.getcwd(), title="Select folder with CSV files")
+        self.directory = root.directory
+
+    def iterateCSVThroughFolder(self):
+        for file in os.listdir(self.directory):
+            filestring = os.fsdecode(file)
+            if filestring.endswith(".csv"):
+                # yield os.path.join(self.directory, filestring)
+                yield self.directory, filestring
+            else:
+                continue
 
 
 class CSVFile:
 
-    def __init__(self):
-        self.directory = directory = os.fsencode(directory_in_str)
-        self.filename = os.path.basename(root.PathAndFilename)
-
-    def iterateCSVThroughFolder(self):
-        # directory = CSVFile.getFilePath()[0]
-        # print(directory)
-        for file in os.listdir(self.directory):
-            filename = os.fsdecode(file)
-            if filename.endswith(".csv"):
-                print('ok')
-                print(os.path.join(self.directory, filename))
-                # return os.path.join(directory, filename)
-                continue
-            else:
-                print('no')
-                continue
+    def __init__(self, directory, name):
+        self.directory = directory
+        self.name = name
 
     def read(self):
-        with open(self.name) as file:
+        with open(os.path.join(self.directory, self.name)) as file:
             reader = csv.reader(file, delimiter=',')
             for row in reader:
                 yield row
 
+    def getLabParams(self):
+        name = self.name.split('.')[0]
+        exposure, during, acquisition, what = name.split('-')[0].split(' ')
+        exposure = float(exposure.replace('s', ''))
+        return name, exposure
+
     def getData(self):
-        xdata = []
-        ydata = []
+        number = []
+        mean = []
+        deviation = []
         for row in self.read():
             try:
                 float(row[0])
             except ValueError:
                 continue
             else:
-                print(row)
-            yield xdata, ydata
+                number.append(float(row[0]))
+                mean.append(float(row[1]))
+                deviation.append(float(row[2]))
+        return number, mean, deviation
 
 
-root = tk.Tk()
-root.withdraw()
-root.PathAndFilename = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select HDF5 file",
-                                                  filetypes=(("CSVfiles", "*.csv"), ("all files", "*.*")))
-directory = os.path.split(root.PathAndFilename)[0] + '/'
-filename = os.path.basename(root.PathAndFilename)
+if __name__ == '__main__':
+    for file in Folder().iterateCSVThroughFolder():
+        CSV = CSVFile(file[0], file[1])
+        noExtension, theExposure = CSV.getLabParams()
+        xdata, ydata, deviationdata = CSV.getData()
 
-print(filename)
+        x = [i * theExposure for i in xdata]
+        y = [i/max(ydata) for i in ydata]
+        dev = round(numpy.mean([i/max(ydata) for i in deviationdata]), 2)
 
-# for i in x.read():
-#     print(i)
+        plt.plot(x, y, label='%s (std dev = %s)' % (noExtension, dev))
+
+    plt.xlabel('Time [s]')
+    plt.ylabel('Intensity (normalised)')
+    plt.legend()
+    plt.show()

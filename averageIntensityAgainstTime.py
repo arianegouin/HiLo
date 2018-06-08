999bee894c72f40362d33bb77995e725edd5543e
arianegouin <39052797+arianegouin@users.noreply.github.com>
2018-06-08 15:22:36 -0400

    add data analysis

--- averageIntensityAgainstTime.py
+++ averageIntensityAgainstTime.py
@@ -39,7 +39,11 @@ class CSVFile:
     def getLabParams(self):
         name = self.name.split('.')[0]
         exposure, during, acquisition, what = name.split('-')[0].split(' ')
-        exposure = float(exposure.replace('s', '').replace('m', ''))
+        exposure = exposure.replace('s', '')
+        if exposure.find('m') != -1:
+            exposure = float(exposure.replace('m', '')) / 1000
+        else:
+            exposure = float(exposure)
         return name, exposure
 
     def getData(self):
@@ -62,18 +66,43 @@ if __name__ == '__main__':
     numberOfFile = 0
 
     for file in Folder().iterateCSVThroughFolder():
-        numberOfFile += 1
+        # numberOfFile += 1
+        shift = -numberOfFile
+
+        # if (numberOfFile % 4) == 1:
+        #     shift = 3
+        # elif (numberOfFile % 4) == 2:
+        #     shift = 2
+        # elif (numberOfFile % 4) == 3:
+        #     shift = 1
+        # elif (numberOfFile % 4) == 0:
+        #     shift = 0
+        # else:
+        #     continue
+
+        # if (numberOfFile % 2) == 1:
+        #     shift = 1
+        # elif (numberOfFile % 2) == 0:
+        #     shift = 0
+        # else:
+        #     continue
+
 
         CSV = CSVFile(file[0], file[1])
         noExtension, theExposure = CSV.getLabParams()
         xdata, ydata, deviationdata = CSV.getData()
 
-        x = [i * theExposure + numberOfFile for i in xdata]
-        y = [i/max(ydata) + numberOfFile for i in ydata]
+        x = [i * theExposure for i in xdata]
+        y = [i/max(ydata) + shift for i in ydata]
         dev = round(numpy.mean([i/max(ydata) for i in deviationdata]), 2)
 
         plt.plot(x, y, label='%s (std dev = %s)' % (noExtension, dev))
 
+plt.xlabel('Time [s]')
+plt.ylabel('Intensity (normalised)')
+plt.ylim((0, 1.1))
+plt.legend()
+plt.show()
 
         # if (numberOfFile % 2) == 0:
         #     plt.xlabel('Time [s]')

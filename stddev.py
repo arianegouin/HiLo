c9b017eac8e91354e87d984813f6b0e29de3c74a
arianegouin <39052797+arianegouin@users.noreply.github.com>
2018-06-18 16:38:27 -0400

    commit

---
+++ stddev.py
@@ -0,0 +1,80 @@
+from PIL import Image
+import numpy
+from matplotlib import pyplot as plt
+
+
+class TiffImage:
+
+    def __init__(self, path):
+        self.image = Image.open('%s' % path)
+
+    def show(self):
+        self.image.show()
+
+    def returnArray(self):
+        return numpy.array(self.image, dtype='float64')
+
+    def turnIntoArray(self):
+        array = self.returnArray()
+        return TiffArray(array)
+
+
+class TiffArray:
+
+    def __init__(self, array):
+        self.array = array
+
+        self.shape = self.array.shape
+
+    def show(self):
+        print(self.array)
+
+    def sumAllPixels(self):
+        return self.array.sum()
+
+    def normalise(self):
+        self.array = self.array / self.sumAllPixels()
+
+    def saveImage(self, name):
+        return Image.fromarray(self.array).save('%s' % name)
+
+
+class StackArray:
+
+    def __init__(self, stack1, *args):
+        self.stack = numpy.stack((stack1, *args), axis=0)
+
+        self.shape = self.stack.shape
+
+    def stddev(self):
+        return numpy.std(self.stack, axis=0)
+
+
+
+image0 = TiffImage('0.tiff')
+array0 = image0.turnIntoArray()
+array0.normalise()
+array0.saveImage('your_file.tiff')
+
+yourfile = TiffImage('your_file.tiff')
+yourfile = yourfile.returnArray()
+
+stack = StackArray(yourfile, yourfile, yourfile)
+std = stack.stddev()
+
+
+
+image0 = TiffImage('0.tiff')
+array0 = image0.returnArray()
+
+image1 = TiffImage('1.tiff')
+array1 = image1.returnArray()
+
+image2 = TiffImage('2.tiff')
+array2 = image2.returnArray()
+
+stack = StackArray(array0, array1, array2)
+std = stack.stddev()
+std = std.ravel()
+
+# plt.hist(std, bins=100)
